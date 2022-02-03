c54fb2087714c1fcd1ee2ca634a72bd59ddba86b
Artem BLANAR <ablanar@e1r12p23.42.fr>
2022-02-03 18:19:31 +0100

    histo

--- describe.py
+++ describe.py
@@ -41,6 +41,4 @@ class describe:
         return d0+d1
         
 if __name__ == '__main__':
-    # df = pd.read_csv('datasets/dataset_train.csv')
     describe('datasets/dataset_train.csv')
-    print(pd.read_csv('datasets/dataset_train.csv').describe())
